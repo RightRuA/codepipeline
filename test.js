aaf4e92e1d4f51a441423470fd11810842ae17b5
RaRu <mental0421@gmail.com>
2025-11-19 10:11:42 +0900

    Change test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-console.log("hello, Cloud Computing!")
+console.log("hellom World!")
+console.log("hello, RaRu!")
+console.log("hellom Dongyang!")
 const now = new Date();
-console.log(now.toString());
-console.log(now.toLocaleTimeString())
+console.log(now.toString())
+console.log(now.toLocaleDateString)
